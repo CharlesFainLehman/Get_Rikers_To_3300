7aaf90bdcd9778c106d766f05478373012fcdcb2
CharlesFainLehman <charles.fain.lehman@gmail.com>
2022-09-07 09:47:20 -0400

    rough app

--- app.R
+++ app.R
@@ -1,7 +1,12 @@
 library(shiny)
 library(tidyverse)
 
-dic <- left_join(read.csv("dat/Daily_Inmates_In_Custody.csv"), read.csv("dat/charge features.csv"), by = c('TOP_CHARGE' = 'Code'))
+dic <- left_join(read.csv("dat/Daily_Inmates_In_Custody.csv"), read.csv("dat/charge features.csv"), by = c('TOP_CHARGE' = 'Code')) %>%
+  mutate(RACE = recode(RACE, "O" = "Other", "B" = "Black", "W" = "White", "I" = "Indian", "A" = "Asian", "U" = "Unknown"),
+         GENDER = recode(GENDER, "M" = "Male", "F" = "Female"),
+         FMV = ifelse(is.na(FMV), "None", recode(FMV, "F" = "Felony", "M" = "Misdemeanor", "V" = "Violation")),
+         Type = ifelse(is.na(Type), "None", Type),
+         CUSTODY_LEVEL = recode(CUSTODY_LEVEL, "MIN" = "Minimum", "MED" = "Medium", "MAX" = "Maximum"))
 todays.pop <- nrow(dic)
 
 ui <- fluidPage(
@@ -11,7 +16,7 @@ ui <- fluidPage(
       selectInput('race', "Race", c("Asian", "Black", "Indian", "Other", "Unknown", "White"), multiple = T, selected = c("Asian", "Black", "Indian", "Other", "Unknown", "White")),
       selectInput('sex', "Sex", c("Male", "Female"), multiple = T, selected = c("Male", "Female")),
       sliderInput('age', "Age", min = 0, max = 100, c(0, 100)),
-      selectInput('fmv', "Offense", c("Felon", "Misdemeanor", "Violation"), multiple = T, selected = c("Felon", "Misdemeanor", "Violation")),
+      selectInput('fmv', "Offense", c("Felony", "Misdemeanor", "Violation"), multiple = T, selected = c("Felony", "Misdemeanor", "Violation")),
       selectInput('type', "Type", c("Narcotics", "Sex Offense", "Violent", "Weapons", "Vehicle", "Misc", "Property"), multiple = T, selected = c("Narcotics", "Sex Offense", "Violent", "Weapons", "Vehicle", "Misc", "Property")),
       selectInput('custody', "Custody Level", c("Minimum", "Medium", "Maximum"), multiple = T, selected = c("Minimum", "Medium", "Maximum")),
       checkboxInput('srg', "Include Prison Gang Members", value = T),
@@ -26,22 +31,30 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  dic.summary <- eventReactive(input$update, {
+  dic.summary <- eventReactive(input$update, ignoreNULL = F, {
     new.length <- dic %>%
-      filter(AGE %in% input$age[1]:input$age[2]) %>%
+      filter(RACE %in% c(input$race, ""),
+             GENDER %in% c(input$sex, ""),
+             AGE %in% input$age[1]:input$age[2],
+             FMV %in% c(input$fmv, "None"),
+             Type %in% c(input$type, "None"),
+             CUSTODY_LEVEL %in% c(input$custody, "")
+             ) %>%
       nrow()
     
-    data.frame(a = factor(c("Today's Population", "Your version", "3,300"),
+    data.frame(version = factor(c("Today's Population", "Your version", "3,300"),
                           levels = c("Today's Population", "Your version", "3,300")),
-               b = c(todays.pop, new.length, 3300))
+               count = c(todays.pop, new.length, 3300))
   })
   
   output$hist <- renderPlot({
-    ggplot(dic.summary(), aes(x=a, y=b)) +
+    ggplot(dic.summary(), aes(x=version, y=count)) +
       geom_col(fill = "#55A5DA") + 
+      geom_text(aes(label = scales::comma(count), y = count + 120)) +
       scale_y_continuous(labels = scales::comma) +
       theme_minimal() + 
-      theme(panel.grid = element_blank())
+      theme(panel.grid = element_blank(),
+            axis.title = element_blank()) 
   }, res = 96)
 }
 
