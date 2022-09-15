2bc6a5554129a28a39f51ea65fbaad8349b2ec1a
CharlesFainLehman <charles.fain.lehman@gmail.com>
2022-09-15 12:12:17 -0400

    secret

--- bin/upload_to_site.R
+++ bin/upload_to_site.R
@@ -1,4 +1,4 @@
-print(commandArgs(trailingOnly=TRUE)[1]))
+print(commandArgs(trailingOnly=TRUE)[1])
 #library(rsconnect)
 #rsconnect::setAccountInfo(name='charlesfainlehman', token='6EA6B48E0C14C5CC47C55277A75548C3', secret='DRPZxMJmXfgUhK4HdUlBgbuGBQ8k28zk74k0ETV0')
 #print("Account Info Set")
