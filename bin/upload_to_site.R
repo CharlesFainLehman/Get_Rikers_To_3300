1248e7c2bd4c59a65abf47c25c7f14953d8f718f
CharlesFainLehman <charles.fain.lehman@gmail.com>
2022-09-15 11:22:32 -0400

    dealing with secret later

--- bin/upload_to_site.R
+++ bin/upload_to_site.R
@@ -1,3 +1,4 @@
 library(rsconnect)
-rsconnect::setAccountInfo(name='charlesfainlehman', token='6EA6B48E0C14C5CC47C55277A75548C3', secret=commandArgs(trailingOnly=TRUE)[1])
+rsconnect::setAccountInfo(name='charlesfainlehman', token='6EA6B48E0C14C5CC47C55277A75548C3', secret='DRPZxMJmXfgUhK4HdUlBgbuGBQ8k28zk74k0ETV0')#commandArgs(trailingOnly=TRUE)[1])
+print("Account Info Set")
 rsconnect::deployApp()
