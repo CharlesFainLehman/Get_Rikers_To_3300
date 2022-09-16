18a9da6ac3d8912be46b99a44cd27b7261ddf68c
CharlesFainLehman <charles.fain.lehman@gmail.com>
2022-09-16 09:52:52 -0400

    Fixing a case issue

--- app.R
+++ app.R
@@ -6,17 +6,19 @@ library(ggplot2)
 #so that excel doesn't screw it up every time I open it
 #Yes, I know, a mess
 charges <- read.csv("dat/charge features.csv") %>% mutate(Code = gsub("'", "", Code))
-dic <- left_join(read.csv("dat/Daily_Inmates_In_Custody.csv"), charges, by = c('TOP_CHARGE' = 'Code')) %>%
-  mutate(RACE = recode(RACE, "O" = "Other", "B" = "Black", "W" = "White", "I" = "Indian", "A" = "Asian", "U" = "Unknown"),
-         GENDER = recode(GENDER, "M" = "Male", "F" = "Female"),
-         AGE = as.numeric(AGE),
-         AGE = ifelse(is.na(AGE), -1, AGE),
-         INMATE_STATUS_CODE = recode(INMATE_STATUS_CODE, "CS" = "City Sentenced", "CSP" = "City Sentenced", "DE" = "Pretrial Detainee", "DEP" = "Pretrial Criminal Parole", "DNS" = "State Sentenced", "DPV" = "Pretrial Technical Parole", "SCO"= "State Sentenced", "SSR" = "State Sentenced"),
+dic <- read.csv("dat/Daily_Inmates_In_Custody.csv") %>% 
+  rename_with(tolower) %>% #uh sometimes DOC changes whether the variables are upper or lower case? Dumb!
+  left_join(charges, by = c('top_charge' = 'Code')) %>%
+  mutate(race = recode(race, "O" = "Other", "B" = "Black", "W" = "White", "I" = "Indian", "A" = "Asian", "U" = "Unknown"),
+         gender = recode(gender, "M" = "Male", "F" = "Female"),
+         age = as.numeric(age),
+         age = ifelse(is.na(age), -1, age),
+         inmate_status_code = recode(inmate_status_code, "CS" = "City Sentenced", "CSP" = "City Sentenced", "DE" = "Pretrial Detainee", "DEP" = "Pretrial Criminal Parole", "DNS" = "State Sentenced", "DPV" = "Pretrial Technical Parole", "SCO"= "State Sentenced", "SSR" = "State Sentenced"),
          FMV = as.character(FMV),
          FMV = ifelse(is.na(FMV), "None", recode(FMV, "F" = "Felony", "M" = "Misdemeanor", "V" = "Violation")),
          Type = as.character(Type),
          Type = ifelse(is.na(Type), "None", Type),
-         CUSTODY_LEVEL = recode(CUSTODY_LEVEL, "MIN" = "Minimum", "MED" = "Medium", "MAX" = "Maximum"))
+         custody_level = recode(custody_level, "MIN" = "Minimum", "MED" = "Medium", "MAX" = "Maximum"))
 todays.pop <- nrow(dic)
 
 ui <- fluidPage(
@@ -50,15 +52,15 @@ server <- function(input, output, session) {
   dic.summary <- reactive({
 
     new.length <- dic %>%
-      filter(RACE %in% c(input$race, ""),
-             GENDER %in% c(input$sex, ""),
-             AGE %in% c(input$age[1]:input$age[2], -1),
-             INMATE_STATUS_CODE %in% c(input$sentence, ""),
+      filter(race %in% c(input$race, ""),
+             gender %in% c(input$sex, ""),
+             age %in% c(input$age[1]:input$age[2], -1),
+             inmate_status_code %in% c(input$sentence, ""),
              FMV %in% c(input$fmv, "None"),
              Type %in% c(input$offense, "None"),
-             CUSTODY_LEVEL %in% c(input$custody, ""),
-             if(input$srg == F) {SRG_FLG != "Y"} else {SRG_FLG %in% c("Y", "N", "")},
-             if(input$bradh == F) {BRADH != "Y"} else {BRADH %in% c("Y", "N", "")}
+             custody_level %in% c(input$custody, ""),
+             if(input$srg == F) {srg_flg != "Y"} else {srg_flg %in% c("Y", "N", "")},
+             if(input$bradh == F) {bradh != "Y"} else {bradh %in% c("Y", "N", "")}
              ) %>%
       nrow()
     
