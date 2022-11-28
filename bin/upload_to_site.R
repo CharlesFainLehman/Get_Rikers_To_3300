156c674820b2dbf5924e5e47c90e2f1f4dd479a5
CharlesFainLehman <charles.fain.lehman@gmail.com>
2022-11-28 12:45:25 -0500

    switching to MI shinyapps

--- bin/upload_to_site.R
+++ bin/upload_to_site.R
@@ -1,3 +1,3 @@
 library(rsconnect)
-rsconnect::setAccountInfo(name='charlesfainlehman', token='CC92133ADD7968271212E7CA8DE1FD8A', secret=commandArgs(trailingOnly=TRUE)[1])
+rsconnect::setAccountInfo(name='manhattan-institute', token='0AFD0942A9173E026C2B630AD17DE92F', secret=commandArgs(trailingOnly=TRUE)[1])
 rsconnect::deployApp()
